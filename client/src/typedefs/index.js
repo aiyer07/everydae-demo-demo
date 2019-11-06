2fc5f41572aaab2caa4d359fd19744db19631262
Arjun <arjun@getwisely.com>
2019-11-06 10:34:55 -0800

    Update index.js

--- client/src/typedefs/index.js
+++ client/src/typedefs/index.js
@@ -1,10 +1,4 @@
 import gql from 'graphql-tag';
 
 export const typeDefs = gql`
-  extend type Query {
-    question: [Question]
-  }
-  # extend type Mutation {
-  #   addOrRemoveFromCart(id: ID!): [Launch]
-  # }
 `;
